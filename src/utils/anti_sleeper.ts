fbd5862a0f5a10921906c39b36847a2ceefa4130
Filip Ksobiak <filip456781337@gmail.com>
2022-12-05 17:45:46 +0100

    anti sleeper

--- src/utils/anti_sleeper.ts
+++ src/utils/anti_sleeper.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 export function antiSleeper() {
   setInterval( () => {
     let PORT = process.env.PORT || 3001;
-    axios.get(`http://10.0.2.2:${PORT}/`);
+    axios.get(`https://libras.onrender.com`);
     logger.info('## anti sleeper ##');
   }, 10*1000*60);
 }
